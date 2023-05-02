3cbbd6eb7ee45ef699f71ff8d41d129fa18c0db4
David Lee <lee.david0706@gmail.com>
2023-05-02 14:07:33 -0400

    Edits

---
+++ SearchBook/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, Output } from '@angular/core';
+import { BookList } from './interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'SearchBook';
+}
---
+++ SearchBook/src/app/interface.ts
@@ -0,0 +1,7 @@
+export interface BookList {
+    picture? : string,
+    title? : string,
+    publisher? : string,
+    pubDate? : string,
+    description? : string,
+}
