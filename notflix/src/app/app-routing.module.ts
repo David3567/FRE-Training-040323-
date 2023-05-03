5b262f08e1b625980c326f14fab1edd712e15508
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-05-03 10:14:55 -0500

    implemented login and homepage

---
+++ notflix/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'signin', component: SignInComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
