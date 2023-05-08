a20c9b99e39fc4330dea687cf80b0a4f554188e3
lukeyzhong <lukeyzhong@gmail.com>
2023-05-08 00:27:55 -0500

    UPDATE

--- form/src/app/app.module.ts
+++ form/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SelectAllComponent } from './select-all/select-all.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { ReactiveComponent } from './reactive/reactive.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -11,7 +10,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    SelectAllComponent,
     TemplateDrivenComponent,
     ReactiveComponent
   ],
