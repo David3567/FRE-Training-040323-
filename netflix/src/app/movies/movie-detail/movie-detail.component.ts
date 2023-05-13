afc10f321ac756b242c00e87b87646486d7d9fd4
Daniel <t.dung.lhp@gmail.com>
2023-05-13 18:23:08 -0500

    fetch movie data to movie page

--- netflix/src/app/movies/movie-detail/movie-detail.component.ts
+++ netflix/src/app/movies/movie-detail/movie-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map, Observable } from 'rxjs';
 
@@ -7,8 +7,12 @@ import { map, Observable } from 'rxjs';
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css'],
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
-
-  id$ = this.route.paramMap.pipe(map((param) => param.get('id')));
+  id!: string | null;
+  data: any;
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.data = history.state.data;
+  }
 }
--- netflix/src/app/movies/movie/movie.component.ts
+++ netflix/src/app/movies/movie/movie.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from './movie.interface';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
@@ -9,7 +11,19 @@ import { Movie } from './movie.interface';
 export class MovieComponent {
   @Input() movieDetails!: Movie;
 
+  constructor(private http: HttpClient, private router: Router) {}
   onButtonClick() {
     console.log(this.movieDetails.id);
+  }
+
+  getDataAndNavigate() {
+    this.http
+      .get(
+        `https://api.themoviedb.org/3/movie/${this.movieDetails.id}?api_key=9f7b20416809f982ba3dd585db30907b&language=en-US`
+      )
+      .subscribe((data: any) => {
+        const id = data.id;
+        this.router.navigate(['/movies', id], { state: { data } });
+      });
   }
 }
